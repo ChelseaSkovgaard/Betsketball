624d02f35c14ef0f7de56a62cb15f54e17cb321f
Mike Ziccardi <mikeziccardi@gmail.com>
2016-11-03 11:03:25 -0600

    adds functionality of uuid function to return a key

--- test/index.js
+++ test/index.js
@@ -8,9 +8,9 @@ describe('createUUID', () => {
   it('it is a function', () => {
     assert.isFunction(createUUID)
   })
-  it('should return cats', () => {
-    let cats = createUUID()
-    assert.equal(cats,'cats')
+  it('should return a key', ()=>{
+    let game ={date: "2016-11-03", time: "7:00PM", awayTeam: Object, homeTeam: Object, location: "Time Warner Cable Arena"}
+    let key = "Time-Warner-Cable-Arena-2016-11-03"
+    assert.equal(createUUID(game),key)
   })
-
 })
